e1666cf527d6bb8f24a97ce70451c6ee33e638fe
youssef shaaban <youssefshahban2001@gmail.com>
2026-05-25 19:16:21 +0300

    add deploy file

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
