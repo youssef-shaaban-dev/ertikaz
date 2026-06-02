5eebec2e76ae48aae6c58c1e2afc30d4ba942418
youssef shaaban <youssefshahban2001@gmail.com>
2026-06-02 12:44:15 +0300

    add googleTagManager Code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
+import Script from "next/script";
 import HeaderSection from "@/components/layout/HeaderSection";
 import FooterSection from "@/components/layout/FooterSection";
 import StickyWhatsapp from "@/components/layout/StickyWhatsapp";
@@ -62,7 +63,26 @@ export default async function LocaleLayout({
       dir={isRtl ? "rtl" : "ltr"}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
+      <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MZTJJG9K');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-linear-to-b from-[#f0f9ff] via-[#ffffff] to-[#e6f4fe] text-blue-950 overflow-x-hidden font-sans selection:bg-blue-600 selection:text-white">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZTJJG9K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider messages={messages}>
           <HeaderSection locale={locale} />
           <main className="grow bg-linear-to-b from-[#f0f9ff] via-[#ffffff] to-[#e6f4fe] text-blue-950 relative overflow-x-hidden selection:bg-blue-600 selection:text-white">
